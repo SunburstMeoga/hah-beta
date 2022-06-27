5b72d124505e68d21a3c0c71d19a0376cff7dcc8
apple <apple@192.168.2.112>
2022-06-27 16:36:06 +0800

    first commit

---
+++ src/popup/components/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="popup-title">
+      <div class="popup-title-left">hello</div>
+      <div class="popup-title-right">world</div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .popup-title {
+        font-size: 20px;
+        &-left {
+            color: red;
+        }
+        &-right {
+            color: blue;
+        }
+    }
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,59 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+// Copy files to specified directory
+const copyFiles = [
+ {
+     from: path.resolve("src/plugins/manifest.json"),
+     to: `${path.resolve("dist")}/manifest.json`
+   },
+   {
+     from: path.resolve("src/assets"),
+     to: path.resolve("dist/assets")
+   },
+   {
+     from: path.resolve("src/plugins/inject.js"),
+     to: path.resolve("dist/js")
+   }
+];
+
+// copy plugin
+const plugins = [
+   new CopyWebpackPlugin({
+     patterns: copyFiles
+   })
+];
+
+// page file
+const pages = {};
+// config popup.html
+const chromeName = ["popup"];
+
+chromeName.forEach(name => {
+   pages[name] = {
+     entry: `src/${name}/main.js`,
+     template: `src/${name}/index.html`,
+     filename: `${name}.html`
+   };
+});
+
+module.exports = {
+ pages,
+ productionSourceMap: false,
+ // config content.js background.js
+ configureWebpack: {
+  entry: {
+   background: "./src/background/main.js"
+  },
+  output: {
+   filename: "js/[name].js"
+  },
+  plugins
+ },
+ // config content.css
+ css: {
+  extract: {
+   filename: "css/[name].css"
+  }
+ }
+}
