0c8fad1e7180ecf5337a128292016568cc84ff58
apple <apple@appledeMacBook-Air.local>
2022-07-21 17:21:32 +0800

    创建/导入钱包流程

---
+++ src/popup/components/scanCodePopup.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container-code">
+    <div class="code">
+        <qrcode-vue :value="value" :size="size" level="H" />
+    </div>
+  </div>
+</template>
+
+<script>
+import QrcodeVue from 'qrcode.vue'
+export default {
+  props:{
+    value:{
+      type:String,
+      require:true
+    },
+    size:{
+      type:Number,
+      default:300
+    }
+  },
+  components:{
+    QrcodeVue
+  },
+}
+</script>
+
+<style>
+.container-code {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: #000;
+  opacity: 0.8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.code {
+    background: #fff;
+    padding: 6px;
+}
+</style>
--- src/popup/pages/mnemonicPage.vue
+++ src/popup/pages/mnemonicPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="title">请输入助记词</div>
+    <div class="title">{{ pageTitle }}</div>
     <div class="content-list">
       <div
         class="content-list-all"
@@ -15,17 +15,33 @@
           <input
             type="text"
             :autofocus="index === 0 && _index === 0"
+            :disabled="!canEnter"
             v-model="_item.content"
           />
         </div>
       </div>
+    </div>
+    <div class="address" v-show="showAddressInfo">
+        <div class="address-title">
+            钱包地址
+        </div>
+        <div class="address-content">{{ address }}</div>
+        <div class="address-operate">
+            <div class="address-operate-button address-operate-copy" @click="copyAddress()">复制钱包地址</div>
+            <div class="address-operate-button address-operate-code" @click="scanCode()">生成二维码</div>
+        </div>
+        <div class="scan-code" v-show="showScanCode">
+            <scan-code-popup :value="address"></scan-code-popup>
+        </div>
     </div>
     <div class="confirm" @click="clickConfirm()">确认</div>
   </div>
 </template>
 
 <script>
+import ScanCodePopup from '@/popup/components/scanCodePopup.vue'
 export default {
+  components: { ScanCodePopup },
   data() {
     return {
       contentList: [
@@ -37,40 +53,36 @@ export default {
       mnemonic: "",
       address: "",
       privateKey: "",
-      type: ''
+      type: "",
+      pageTitle: "",
+      canEnter: true,
+      showScanCode: false,
+      showAddressInfo: false
     };
   },
   mounted() {
-    this.type = this.$route.query.type
-    console.log('当前创建钱包方式', this.type)
+    this.type = this.$route.query.type;
+    console.log("当前创建钱包方式", this.type);
+    if (this.type === "create") {
+      //如果是创建新的钱包地址，则生成助记词和地址
+      this.pageTitle = "请牢记助记词（创建钱包）";
+      this.generateMnemonicsAndAddresses();
+      this.canEnter = false;
+      this.showAddressInfo = true
+    } else {
+      this.pageTitle = "请输入助记词（导入钱包）";
+      this.canEnter = true
+      this.showAddressInfo = false
+    }
   },
   methods: {
-    // generateMnemonic(strength, rng, wordlist) {
-    //   // 空参时长度默认为128，rng为randomBytes方法，wordlist为默认值
-    //   strength = strength || 128;
-    //   if (strength % 32 !== 0) throw new TypeError(INVALID_ENTROPY);
-    //   rng = rng || randomBytes; // randomBytes(size)在第size大于0小于MAX_BYTES时，返回一个以随机数填充，长为size的Buffer
-    //   // 通过 rng = randomBytes，rng(strength / 8)获得了一个长为16，以随机数填充的Buffer，entropyToMnemonic基于该Buffer生成助记词
-    //   return entropyToMnemonic(rng(strength / 8), wordlist);
-    // },
-    clickConfirm() {
-      // let bip39 = require("bip39");
-      // let { hdkey } = require("ethereumjs-wallet");
-      // let util = require("ethereumjs-util");
-      let ethers = require('ethers')
+    // 生成助记词和地址
+    generateMnemonicsAndAddresses() {
+      let ethers = require("ethers");
       var wallet = ethers.Wallet.createRandom();
-      // let mnemonic = bip39.generateMnemonic(128);
-      // mnemonic = bip39.validateMnemonic(mnemonic)
-      // let seed = bip39.mnemonicToSeedSync(wallet.mnemonic);
-      // let hdWallet = hdkey.fromMasterSeed(seed);
-      // let key = hdWallet.derivePath("m/44'/60'/0'/0/0");
-      // let address = util.toChecksumAddress(
-      //   "0x" + util.pubToAddress(key._hdkey._publicKey, true).toString("hex")
-      // );
-
-      // this.mnemonic = wallet.mnemonic;
-      // this.mnemonic = mnemonic
       let arr = wallet.mnemonic.phrase.trim().split(/\s+/);
+      this.address = wallet.address;
+      this.privateKey = wallet.privateKey;
       this.contentList[0][0].content = arr[0];
       this.contentList[0][1].content = arr[1];
       this.contentList[0][2].content = arr[2];
@@ -83,18 +95,75 @@ export default {
       this.contentList[3][0].content = arr[9];
       this.contentList[3][1].content = arr[10];
       this.contentList[3][2].content = arr[11];
-      this.privateKey = wallet.privateKey;
+
       console.log(this.address);
-      // this.address = address;
-      // this.test = hdkey
-      // this.ethers = ethers
     },
+    //根据私钥和助记词找回钱包地址
+    importWalletAddress() {
+      let arr = []
+      this.contentList.map(item => {
+          item.map(_item => {
+              arr.push(_item.content)
+          })
+      })
+      let mnemonicStr = '';
+      mnemonicStr = arr.join(' ')
+      var ethers = require("ethers");
+      var mnemonic = ethers.Wallet.fromMnemonic(mnemonicStr);
+      var privateKey = mnemonic.privateKey;
+      var wallet = new ethers.Wallet(privateKey);
+      var address = wallet.address;
+      this.address = address
+      this.showAddressInfo = true
+      console.log("钱包地址：", address);
+      console.log("钱包私钥：", privateKey);
+    },
+    //复制钱包地址
+    copyAddress() {
+        navigator.clipboard.writeText(this.address);
+        alert('复制成功')
+    },
+    scanCode() {
+        this.showScanCode = true
+    },
+    //点击底部确认按钮
+    clickConfirm() {
+        this.importWalletAddress()
+    },
+    // generateMnemonic(strength, rng, wordlist) {
+    //   // 空参时长度默认为128，rng为randomBytes方法，wordlist为默认值
+    //   strength = strength || 128;
+    //   if (strength % 32 !== 0) throw new TypeError(INVALID_ENTROPY);
+    //   rng = rng || randomBytes; // randomBytes(size)在第size大于0小于MAX_BYTES时，返回一个以随机数填充，长为size的Buffer
+    //   // 通过 rng = randomBytes，rng(strength / 8)获得了一个长为16，以随机数填充的Buffer，entropyToMnemonic基于该Buffer生成助记词
+    //   return entropyToMnemonic(rng(strength / 8), wordlist);
+    // },
+    // clickConfirm() {
+    //   let bip39 = require("bip39");
+    //   let { hdkey } = require("ethereumjs-wallet");
+    //   let util = require("ethereumjs-util");
+
+    //   let mnemonic = bip39.generateMnemonic(128);
+    //   mnemonic = bip39.validateMnemonic(mnemonic)
+    //   let seed = bip39.mnemonicToSeedSync(wallet.mnemonic);
+    //   let hdWallet = hdkey.fromMasterSeed(seed);
+    //   let key = hdWallet.derivePath("m/44'/60'/0'/0/0");
+    //   let address = util.toChecksumAddress(
+    //     "0x" + util.pubToAddress(key._hdkey._publicKey, true).toString("hex")
+    //   );
+
+    //   this.mnemonic = wallet.mnemonic;
+    //   this.mnemonic = mnemonic
+
+    //   this.address = address;
+    //   this.test = hdkey
+    //   this.ethers = ethers
+    // },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-
 .title {
   font-size: 16px;
   margin-top: 20px;
@@ -106,7 +175,7 @@ export default {
   align-items: center;
   justify-content: center;
   margin-top: 40px;
-  margin-bottom: 80px;
+  margin-bottom: 20px;
   &-all {
     display: flex;
     justify-content: space-between;
@@ -124,6 +193,42 @@ export default {
       }
     }
   }
+}
+.address {
+    width: 280px;
+    background: #f8f8f8;
+    border: 1px solid #dcdcdc;
+    border-radius: 6px;
+    padding: 10px;
+    margin-bottom: 40px;
+    &-title {
+        font-weight: bold;
+        font-size: 14px;
+         margin-bottom: 6px;
+    }
+    &-content {
+        word-break: break-all;
+        margin-bottom: 6px;
+    }
+    &-operate {
+        display: flex;
+        justify-content: space-around;
+        &-button {
+            width: 80px;
+            height: 20px;
+            border-radius: 20px;
+            border: 1px solid;
+            font-size: 10px;
+            text-align: center;
+            line-height: 20px;
+        }
+        &-copy {
+
+        }
+        &-code {
+
+        }
+    }
 }
 .confirm {
   width: 300px;
